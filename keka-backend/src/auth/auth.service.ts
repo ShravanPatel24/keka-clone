9d3d5e86b7530fc3bdb3672a79441c9a3ae542ae
amit kumar <amit83666@gmail.com>
2023-10-17 15:10:01 +0530

    add bcrypt on password

--- keka-backend/src/auth/auth.service.ts
+++ keka-backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { JwtService } from '@nestjs/jwt';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class AuthService {
   constructor(
@@ -10,7 +10,8 @@ export class AuthService {
   ) {}
   async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.userService.findByEmail(email);
-    if (user && user.password === pass) {
+    pass=await bcrypt.compare(pass, user.password);
+    if (user && pass) {
       const { password, ...result } = user.toObject();
       return result;
     }
--- keka-backend/src/users/users.service.ts
+++ keka-backend/src/users/users.service.ts
@@ -4,12 +4,14 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './models/user.model';
 import { Model } from 'mongoose';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async createUser(user: User): Promise<User> {
+    user.password = await bcrypt.hash(user.password, 10);
     const createUser = new this.userModel(user);
     return createUser.save();
   }
